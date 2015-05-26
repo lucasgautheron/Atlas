1f9de24d89efa97b46581f6b908d7ba2ba55013e
Lucas GAUTHERON <lucas.gautheron@outlook.fr>
2015-05-26 12:01:00 +0200

    ajout attributs

--- macros/tests.c
+++ macros/tests.c
@@ -2,17 +2,38 @@
 #include "TTree.h"
 #include "TMath.h"
 
+struct params
+{
+    bool enable;
+    float E[2];
+    float eta[2];
+};
+
 struct photon
 {
    float E, eta, phi;
    float E_true, eta_true, phi_true;
-   int tight;
+   float true_E, true_eta, true_phi; // ??
+   int tight, loose;
+
+   bool inBounds(params &p)
+   {
+       if(!p.enable) return true;
+       return (E > p.E[0] && E < p.E[1])
+ &&
+              (abs(eta) > p.eta[0] && abs(eta) < p.eta[1]);
+   }
    
 };
 
-float inv_mass(photon &p1, photon &p2)
+float invMass(photon &p1, photon &p2)
 {
     return TMath::Sqrt(2*p1.E*p2.E*(TMath::CosH(p1.eta-p2.eta)-TMath::Cos(p1.phi-p2.phi)));
+}
+
+float deltaR(photon &p1, photon &p2)
+{
+    return sqrt((p1.eta-p2.eta)*(p1.eta-p2.eta) + (p1.phi-p2.phi)*(p1.phi-p2.phi));
 }
 
 void tests()
@@ -20,10 +41,9 @@ void tests()
   TFile* f = TFile::Open("/home/gautheron/Documents/git/Atlas/macros/small.root");  // open the file
   TTree* tree = (TTree*)f->Get("tree");
 
+  float m;
   photon p1, p2;
 
-  float m, ET1, ET2;
-  int isTight1, isTight2;
   tree->SetBranchAddress("mgg", &m); 
   tree->SetBranchAddress("ph1_Et", &p1.E);
   tree->SetBranchAddress("ph2_Et", &p2.E);
@@ -39,20 +59,38 @@ void tests()
   tree->SetBranchAddress("ph1_phi_true", &p1.phi_true);
   tree->SetBranchAddress("ph2_phi_true", &p2.phi_true);
 
+  tree->SetBranchAddress("truePhoton1_Et", &p1.true_E);
+  tree->SetBranchAddress("truePhoton2_Et", &p2.true_E);
+
+  tree->SetBranchAddress("truePhoton1_eta", &p1.true_eta);
+  tree->SetBranchAddress("truePhoton2_eta", &p2.true_eta);
+
   tree->SetBranchAddress("ph1_isTight", &p1.tight);
   tree->SetBranchAddress("ph2_isTight", &p2.tight);
 
-  //TH1F* h = new TH1F("h", "invariant mass", 500, 100, 600); // create a histogram : 500 bins ranging from 100 to 600 GeV.
+  tree->SetBranchAddress("ph1_isLoose", &p1.loose);
+  tree->SetBranchAddress("ph2_isLoose", &p2.loose);
+
+  // TH1F* h = new TH1F("h", "invariant mass", 500, 50, 250); // create a histogram : 500 bins ranging from 100 to 600 GeV.
   TH1F* h = new TH1F("h", "eta", 500, -5, 5);
+  //TH1F* h2 = new TH1F("h2", "eta_true", 500, -5, 5);
+  
+  //h2->SetLineColor(kRed);
+
+  int totalEntries = tree->GetEntries();
+  int looseEntries = 0;
 
   for (unsigned int i = 0; i < tree->GetEntries(); i++) {
     tree->GetEntry(i);
+    if (p1.loose && p2.loose) ++looseEntries;
     if (!p1.tight || !p2.tight) continue;
-    if (p1.E < 50 || p2.E < 50) continue; 
+    //if (p1.E < 50 || p2.E < 50) continue; 
     //printf("%.5f %.5f\n", m, inv_mass(p1, p2));
-    //if(i%2) printf("%.5f %.5f\n", p1.eta_true, p1.eta);
-    h->Fill(p1.eta); // Enter the mass value into the histogram, for the events passing the cuts above
-    h->Fill(p2.eta);
+    //if(deltaR(p1, p2) < 1) printf("%.5f %s %s\n", deltaR(p1, p2), p1.tight ? "t" : "n", p2.tight ? "t" : "n");
+    h->Fill(p1.eta_true); // Enter the mass value into the histogram, for the events passing the cuts above
+    //h2->Fill(p1.eta);
   }
+  printf("%.2f", float(looseEntries)/float(totalEntries));
   h->Draw(); // plot the histogram
+  //h2->Draw();
 }
