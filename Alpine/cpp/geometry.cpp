a81d9058e3643a1d712d9d1b825a641394e1b3cd
Lucas GAUTHERON <lucas.gautheron@outlook.fr>
2015-07-01 17:00:53 +0200

    geometry update

--- Alpine/cpp/geometry.cpp
+++ Alpine/cpp/geometry.cpp
@@ -3,8 +3,11 @@
 #include <TProfile.h>
 #include <TH1.h>
 #include <TH2.h>
+#include "TCanvas.h"
+#include "TPolyLine3D.h"
 #include <TVector.h>
 #include <vector>
+#include "AtlasStyle.C"
 
 #define LAYERS 4
 
@@ -15,8 +18,39 @@
 #define atanh(x) (TMath::ATanH((x)))
 #define sqrt(x) (TMath::Sqrt((x)))
 #define atan(x) (TMath::ATan((x)))
+#define atan2(x,y) (TMath::ATan2((x),(y)))
 #define exp(x) (TMath::Exp((x)))
 #define pi TMath::Pi
+
+TH2 *h_hits_r_z;
+TH2 *h_hits_x_y;
+
+void MatrixToEuler(const TMatrix &mat, TVector3 &vec)
+{
+    // x = theta
+    // y = psi
+    // z = phi
+    if(abs(mat[2][0]) != 1)
+    {
+        vec.SetX(-asin(mat[2][0]));
+        vec.SetY(atan2(mat[2][1]/cos(vec.X()), mat[2][2]/cos(vec.X())));
+        vec.SetZ(atan2(mat[1][0]/cos(vec.X()), mat[0][0]/cos(vec.X())) );
+    }
+    else
+    {
+        vec.SetZ(0);
+        if(abs(mat[2][0]) > 0)
+        {
+            vec.SetX(pi()/2.0);
+            vec.SetY(atan2(mat[0][1], mat[0][2]));
+        }
+        else
+        {
+            vec.SetX(-pi()/2.0);
+            vec.SetY(atan2(-mat[0][1], -mat[0][2]));
+        }
+    }
+}
 
 struct Module
 {
@@ -47,6 +81,8 @@ struct Ray
     float eta, phi, z0;
     
     int hits[LAYERS], totalhits;
+    
+    vector<double *> hitvec;
 };
 
 vector<Module *> modules;
@@ -158,15 +194,24 @@ void trace_rays()
             Module *m = modules[j];
             float t = -(m->d + m->c * r->z0) / (m->a * r->dir.X() + m->b * r->dir.Y() + m->c * r->dir.Z());
             if(t <= 0) continue;
-            TVector3 inter;
+            TVector3 inter, inter_atlas;
             inter.SetX(r->a.X() + r->dir.X() * t);
             inter.SetY(r->a.Y() + r->dir.Y() * t);
             inter.SetZ(r->a.Z() + r->dir.Z() * t);
+            inter_atlas = inter;
             inter = inter - m->pos;
             inter = m->inverse * inter;
             
             if(abs(inter.X()) < m->width/2.0 && abs(inter.Y()) < m->length/2.0)
             {
+                double *hr = new double[3];
+                hr[0] = inter_atlas.X(); hr[1] = inter_atlas.Y(); hr[2] = inter_atlas.Z();
+                //r->hitvec.push_back(hr);
+                
+                if(hr[2] >= 0) h_hits_r_z->Fill( hr[2], sqrt(hr[0]*hr[0]+hr[1]*hr[1]));
+                
+                h_hits_x_y->Fill( hr[0], hr[1]);
+                
                 r->hits[m->layer]++;
                 r->totalhits++;
                 //printf("%d %.3f %.3f %.3f\n", m->layer, inter.X(), inter.Y(), inter.Z());   
@@ -175,22 +220,114 @@ void trace_rays()
     }
 }
 
+void draw_ray(Ray *r)
+{
+    TPolyLine3D *pl = new TPolyLine3D(2);
+    TVector3 dir(0, 0, 0), end(0, 0, 0);
+    dir = r->b - r->a; dir *= 3000 / (dir.Mag());
+    end = r->a + dir;
+    pl->SetPoint(0, r->a.X(), r->a.Y(), r->a.Z());
+    pl->SetPoint(1, end.X(), end.Y(), end.Z());
+    pl->SetLineColor(kRed);
+    pl->Draw();
+    
+    /*TPolyMarker3D *m = TPolyMarker3D(r->hitvec.size());
+    m->SetMarkerColor(kGreen);
+    for(unsigned int i = 0; i < r->hitvec.size(); ++i)
+    {
+        double *h = r->hitvec[i];
+        m->SetPoint(i, h[0], h[1], h[2]);
+    }*/
+}
+
+void render()
+{
+    //TRandom *rando = new TRandom();
+    TGeoManager * geom = new TGeoManager("alpine", "Alpine Layout");
+    TGeoMaterial *vacuumMatter = new TGeoMaterial("Vacuum", 0, 0, 0);
+    TGeoMaterial *sensorMatter = new TGeoMaterial("Sensor"    , .938, 1., 10000.);
+    TGeoMaterial *rayMatter = new TGeoMaterial("Ray", .938, 1., 10000);
+    sensorMatter->SetTransparency(75);
+    vacuumMatter->SetTransparency(100);
+    
+    TGeoMedium *vacuum = new TGeoMedium("vacuum", 1, vacuumMatter);
+    TGeoMedium *sensor = new TGeoMedium("sensor", 1, sensorMatter);
+    TGeoMedium *ray = new TGeoMedium("ray", 1, rayMatter);
+    
+    Double_t worldx = 600.;
+    Double_t worldy = 600.;
+    Double_t worldz = 2000.;
+    
+    TGeoVolume *top = geom->MakeBox("WORLD", vacuum, worldx, worldy, worldz);
+    geom->SetTopVolume(top);
+    
+    TGeoVolume *normal_sensor  = geom->MakeBox("normal_sensor", sensor,  33.6, 19.8, 0.01);
+    TGeoVolume *transition_sensor  = geom->MakeBox("transition_sensor", sensor,  16.8, 19.8, 0.01);
+    TGeoVolume *ray_line = geom->MakeTube("ray_line", ray, 0, 0.01, 1);
+    
+    for(unsigned int i = 0; i < modules.size(); ++i)
+    {
+       Module *m = modules[i];
+       TVector3 angles;
+       MatrixToEuler(m->transform, angles);
+       //angles.SetX(rando->Uniform(-2, 2));
+       //angles.SetY(rando->Uniform(-2, 2));
+       TGeoTranslation trans(m->px, m->py, m->pz);
+       angles *= 180.0/pi();
+       printf("%.3f %.3f %.3f\n", angles.X(), angles.Y(), angles.Z());
+       TGeoRotation rot("euler", angles.Z(), angles.X(), angles.Y());
+       
+       if(m->width > 30) top->AddNode(normal_sensor, i, new TGeoCombiTrans(trans, rot));
+       else top->AddNode(transition_sensor, i, new TGeoCombiTrans(trans, rot));
+    }
+    
+   geom->CloseGeometry();
+   geom->SetVisLevel(4);
+   top->Draw("ogl");
+   
+   for(unsigned int i = 0; i < 1000; ++i) if(abs(rays[i]->eta) > 2.0)
+   {
+       draw_ray(rays[i]);
+       break;
+   }
+   
+    
+}
+
 void geometry()
 {
     load_modules();
     printf("Loaded %d modules\n", (int)modules.size());
     create_rays(10000);
     
+    gStyle->SetOptStat(0);
+    SetAtlasStyle();
+    gStyle->SetOptStat(0);
+    
+    gStyle->SetMarkerSize(0.1);
+    
+    h_hits_r_z = new TH2F("h_hits_r_z", "Hits (r,z)", 1000, 500, 0, 1200, 0, 300);
+    h_hits_x_y = new TH2F("h_hits_x_y", "Hits (x,y)", 1000, -300, 300, 1000, -300, 300);
+    
+    
     trace_rays();
     
     TProfile *p_hits_eta = new TProfile("p_hits_eta", "p_hits_eta", 20, -3, 3, -0.5, 15.5);
     
+    
     for(unsigned int i = 0; i < rays.size(); ++i)
     {
         Ray *r = rays[i];
-        if(r->totalhits < 4) printf("Total hits: %d (%d %d %d %d) (eta = %.2f, phi = %.2f, z0 = %.2f)\n", r->totalhits, r->hits[0], r->hits[1], r->hits[2], r->hits[3], r->eta, r->phi, r->z0);
+        if(r->totalhits < 4)
+        {
+            printf("Total hits: %d (%d %d %d %d) (eta = %.2f, phi = %.2f, z0 = %.2f)\n", r->totalhits, r->hits[0], r->hits[1], r->hits[2], r->hits[3], r->eta, r->phi, r->z0);
+        }
         p_hits_eta->Fill(r->eta, r->totalhits, 1);
     }
     
-    p_hits_eta->Draw();
+    //p_hits_eta->Draw();
+    //h_hits_r_z->Draw();
+    h_hits_x_y->Draw();
+    //render();
+    
 }
