f82517c6734ac21e4d3e85852e23156e6f70ddf7
Lucas GAUTHERON <lucas.gautheron@outlook.fr>
2015-07-23 14:04:23 +0200

    check intersection with environment

--- Alpine/cpp/radiation_2.cpp
+++ Alpine/cpp/radiation_2.cpp
@@ -4,8 +4,11 @@
 #include <TH1.h>
 #include <TH2.h>
 #include <TVector.h>
+#include <vector>
 #include "AtlasStyle.C"
 
+#define max(x,y) (TMath::Max((x),(y)))
+#define min(x,y) (TMath::Min((x),(y)))
 #define cos(x) (TMath::Cos((x)))
 #define sin(x) (TMath::Sin((x)))
 #define cosh(x) (TMath::CosH((x)))
@@ -28,25 +31,66 @@ struct Rectangle
     TVector3 normal;
     double a,b,c,d;
     double width, length;
+    TVector3 max, min;
     TMatrix transform;
-    static const int nx = 40, ny = 20;
+//    static const int nx = 40, ny = 20;
+    static const int nx = 60, ny = 30;
     
+    double power_surface[nx][ny];
     
     double temperature;
     double emissivity;
     double power;
+    
+    void compute_plan()
+    {
+        normal = (A-B).Cross(A-C);
+        normal *= 1/(normal.Mag());
+        
+        a = normal.X();
+        b = normal.Y();
+        c = normal.Z();
+        d = -(a*A.X() + b*A.Y() + c*A.Z());
+        
+        min.SetX(min(min(A.X(), B.X()), min(C.X(), D.X())) - 0.01);
+        min.SetY(min(min(A.Y(), B.Y()), min(C.Y(), D.Y())) - 0.01);
+        min.SetZ(min(min(A.Z(), B.Z()), min(C.Z(), D.Z())) - 0.01);
+            
+        max.SetX(max(max(A.X(), B.X()), max(C.X(), D.X())) + 0.01);
+        max.SetY(max(max(A.Y(), B.Y()), max(C.Y(), D.Y())) + 0.01);
+        max.SetZ(max(max(A.Z(), B.Z()), max(C.Z(), D.Z())) + 0.01);
+    }
 };
+
+bool intersect_box(const TVector3 &a, const TVector3 &b, const Rectangle *r)
+{
+    TVector3 dir = b - a;
+    float length = dir.Mag();
+    dir *= 1/length;
+    float t = -(r->d + r->a * a.X() + r->b * a.Y() + r->c * a.Z()) / (r->a * dir.X() + r->b * dir.Y() + r->c * dir.Z());
+    
+    if(t <= 0) return false;
+    if(t >= length) return false;
+    TVector3 inter;
+    inter.SetX(a.X() + dir.X() * t);
+    inter.SetY(a.Y() + dir.Y() * t);
+    inter.SetZ(a.Z() + dir.Z() * t);
+    
+    return inter.X() >= r->min.X() && inter.Y() >= r->min.Y() && inter.Z() >= r->min.Z() && inter.X() <= r->max.X() && inter.Y() <= r->max.Y() && inter.Z() <= r->max.Z();
+}
 
 struct Cavity
 {
     double radius, length;
-    static const int theta_segments = 150, r_segments = 150, z_segments = 150;
+    static const int theta_segments = 100, r_segments = 150, z_segments = 150;
     static const int rays = 200;
     
     double emissivity;
     double temperature;
     double total_power;
 };
+
+vector<Rectangle *> objects;
 
 void calculate_power(Cavity *c, Rectangle *r)
 {
@@ -104,10 +148,23 @@ void calculate_power(Cavity *c, Rectangle *r)
             {
                 for(int l = 0; l < Rectangle::ny; ++l)
                 {
+                    
                     TVector3 pos;
-                    pos.SetX(-r->width/2.0 + dx * double(k));
-                    pos.SetY(-r->length/2.0 + dy * double(l));
+                    pos.SetX(-r->width/2.0 + dx * (double(k)+0.5));
+                    pos.SetY(-r->length/2.0 + dy * (double(l)+0.5));
                     pos.SetZ(0);
+                    
+                    bool visible = true;
+                    for(int m = 0; m < objects.size(); ++m) if(objects[m] != r)
+                    {
+                        if(intersect_box(origin, pos, objects[m]))
+                        {
+                            visible = false;
+                            break;
+                        }
+                    }
+                    if(!visible) continue;
+                    
                     TVector3 diff = pos - origin;
                     TVector3 n1, n2;
                     n2.SetZ(1); n2.SetX(0); n2.SetY(0);
@@ -115,20 +172,13 @@ void calculate_power(Cavity *c, Rectangle *r)
                     
                     double cos1 = cos(n1.Angle(diff)), cos2 = cos(n2.Angle(diff));
                     
-                    solid += abs(cos1 * cos2 * surface* dx*dy / (pi() * diff.Mag() * diff.Mag()));
+                    double s = abs(cos1 * cos2 * surface* dx*dy / (pi() * diff.Mag() * diff.Mag()));
+                    solid += s;
+                    r->power_surface[k][l] += s;
                 }
             }
-           
-            
-            //double solid = ABS(A+B+C-pi()) + ABS(A_p+B_p+C_p-pi());
-           // printf("%f %f %f %f %f\n", solid, ABS(A+B+C-pi()) + ABS(A_p+B_p+C_p-pi()), origin.X(), origin.Y(), origin.Z());
-            //double solid = 4*pi() * hits/(double(Cavity::rays) * double(Cavity::rays));
-            
-            //printf("%f %f %f\n", (ABS(A+B+C-pi()) + ABS(A_p+B_p+C_p-pi())), solid, r->width * r->length / (origin.Mag() * origin.Mag()));
             
             double power_emitted = SIGMA * pow(c->temperature, 4) * c->emissivity;
-            
-              //      if(ABS(r->power) < 100)    printf("%f %f %f %f %f %f %f %f\n",  alpha, beta, gamma, r->width * r->length  / (4*pi() * origin.Mag() * origin.Mag()), origin.X(), origin.Y(), origin.Z(), r->power);
             
             r->power += power_emitted * solid;
             
@@ -187,9 +237,21 @@ void calculate_power(Cavity *c, Rectangle *r)
                 for(int l = 0; l < Rectangle::ny; ++l)
                 {
                     TVector3 pos;
-                    pos.SetX(-r->width/2.0 + dx * double(k));
-                    pos.SetY(-r->length/2.0 + dy * double(l));
+                    pos.SetX(-r->width/2.0 + dx * (double(k)+0.5));
+                    pos.SetY(-r->length/2.0 + dy * (double(l)+0.5));
                     pos.SetZ(0);
+                    
+                    bool visible = true;
+                    for(int m = 0; m < objects.size(); ++m) if(objects[m] != r)
+                    {
+                        if(intersect_box(origin, pos, objects[m]))
+                        {
+                            visible = false;
+                            break;
+                        }
+                    }
+                    if(!visible) continue;
+                    
                     TVector3 diff = pos - origin;
                     TVector3 n1, n2;
                     n2.SetZ(1); n2.SetX(0); n2.SetY(0);
@@ -197,15 +259,14 @@ void calculate_power(Cavity *c, Rectangle *r)
                     
                     double cos1 = cos(n1.Angle(diff)), cos2 = cos(n2.Angle(diff));
                     
-                    solid += abs(cos1 * cos2 * surface* dx*dy / (pi() * diff.Mag() * diff.Mag()));
+                    double s = abs(cos1 * cos2 * surface* dx*dy / (pi() * diff.Mag() * diff.Mag()));
+                    solid += s;
+                    r->power_surface[k][l] += s;
                 }
             }
-            //printf("%f %f %f %f %f\n", solid, 4*pi() * hits/2500.0, origin.X(), origin.Y(), origin.Z());
-            //double solid = 4*pi() * hits/(double(Cavity::rays) * double(Cavity::rays));
 
             double power_emitted = SIGMA * pow(c->temperature, 4) * c->emissivity;
             
-                   //     printf("%f %f %f %f %f %f\n",  ABS(A+B+C-pi())+ABS(A_p+B_p+C_p-pi()), r->width * r->length  / (4*pi() * origin.Mag() * origin.Mag()), origin.X(), origin.Y(), origin.Z(), r->power);
             
             r->power += power_emitted * solid;
             
@@ -217,32 +278,38 @@ void calculate_power(Cavity *c, Rectangle *r)
 
 void radiation_2()
 {
+    const double MM = 1;
     Rectangle *r = new Rectangle();
-    r->width = 40;
-    r->length = 20;
+    r->width = 40 * MM;
+    r->length = 20 * MM;
     r->power = 0;
     r->A = *new TVector3(-r->width/2.0, -r->length/2.0, 0);
     r->B = *new TVector3(r->width/2.0, -r->length/2.0, 0);
     r->C = *new TVector3(r->width/2.0, r->length/2.0, 0);
     r->D = *new TVector3(-r->width/2.0, +r->length/2.0, 0);
     r->temperature = 273.15 - 30;
+    r->compute_plan();
     
     Rectangle *support = new Rectangle();
-    support->width = 30;
-    support->length = 150;
+    support->width = 40 * MM;
+    support->length = 100 * MM;
     support->power = 0;
-    support->A = *new TVector3(-r->width/2.0, 0, -r->length/2.0);
-    support->B = *new TVector3(r->width/2.0, 0, -r->length/2.0);
-    support->C = *new TVector3(r->width/2.0, 0, r->length/2.0);
-    support->D = *new TVector3(-r->width/2.0, 0, r->length/2.0);
+    support->A = *new TVector3(-support->width/2.0, -r->length/2.0, -support->length/2.0);
+    support->B = *new TVector3(support->width/2.0, -r->length/2.0, -support->length/2.0);
+    support->C = *new TVector3(support->width/2.0, -r->length/2.0, support->length/2.0);
+    support->D = *new TVector3(-support->width/2.0, -r->length/2.0, support->length/2.0);
     support->temperature = 273.15 - 35;
+    support->compute_plan();
+    
+    objects.push_back(r);
+    objects.push_back(support);
     
     Cavity *c = new Cavity();
-    c->radius = 75;
-    c->length = 500;
+    c->radius = 75 * MM;
+    c->length = 500 * MM;
     c->emissivity = 1;
     c->temperature = 273.15 + 26;
-    //calculate_power(c, r);
+    calculate_power(c, r);
     //calculate_power(c, support);
     printf("recv = %e W, approx = %e W, loss = %e W\n", r->power/1e6, 2 * SIGMA * pow(c->temperature, 4) * r->width * r->length / 1e6, 2 * SIGMA * pow(r->temperature, 4) * r->width * r->length / 1e6);
     support->power = 0;
@@ -251,7 +318,7 @@ void radiation_2()
     float min_temp = -40, max_temp = 30;
     float x[num_temp], y[num_temp], z[num_temp];
     
-    for(int k = 0; k < num_temp; ++k)
+    /*for(int k = 0; k < num_temp; ++k)
     {
         x[k] = min_temp + (max_temp - min_temp) * float(k) / float(num_temp);
         c->temperature = 273.15 + x[k];
@@ -264,16 +331,26 @@ void radiation_2()
         //calculate_power(c, support);
         y[k] = (r->power)/1e6;
         z[k] = 2 * SIGMA * pow(r->temperature, 4) * r->width * r->length / 1e6;
-    }
+    }*/
     
     SetAtlasStyle();
     
-     TCanvas *c1 = new TCanvas("c1","...",200,10,700,500);
+     TH2 *h = new TH2F("Radiative power distribution", "Radiative power distribution", Rectangle::nx, 0, Rectangle::nx, Rectangle::ny, 0, Rectangle::ny);
+     
+     for(int k = 0; k < Rectangle::nx; ++k) for(int l = 0; l < Rectangle::ny; ++l)
+     {
+         h->Fill(k, l, double(Rectangle::nx * Rectangle::ny) * SIGMA * pow(c->temperature, 4) * c->emissivity * r->power_surface[k][l] / (r->width * r->length) / 1e6);
+     }
+     
+     printf("%f %f\n", h->GetMinimum(), h->GetMaximum());
+     h->Draw("COLZ");
+    
+    // TCanvas *c1 = new TCanvas("c1","...",200,10,700,500);
      
    //calculate_temperature(r, 0.6);
 
    //c1->SetFillColor(42);
-   c1->SetGrid();
+   /*c1->SetGrid();
 
    TGraph *gr = new TGraph(num_temp,x,y);
    gr->SetLineColor(2);
@@ -292,5 +369,5 @@ void radiation_2()
    gr2->GetYaxis()->SetTitle("P (W)");
    gr2->Draw("sameCP");
    
-   c1->Update();    
+   c1->Update();    */
 }
